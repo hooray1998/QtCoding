da3704cbcafe80c82bb1247cf502e12474e06990
Hooray&T_Tian <32288333+hooray1998@users.noreply.github.com>
2018-04-25 18:50:54 +0800

    Create main.cpp

---
+++ iTT4.8/main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QTextCodec>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("gb2312"));
+    MainWindow w;
+    w.show();
+
+
+    return a.exec();
+    return 0;
+}
